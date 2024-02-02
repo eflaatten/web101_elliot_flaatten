a36d58754ccbb701d3f78aec303ab3ad42f9a611
eflaatten <elliotflaatten@live.com>
2024-02-01 21:06:23 -0600

    starting contact page hw. reorganized files

---
+++ scripts/contact.js
@@ -0,0 +1,8 @@
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
+
+
