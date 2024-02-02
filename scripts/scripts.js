558b333bee7d3bd9ff5783d79edf366c3a1e8c5f
eflaatten <elliotflaatten@live.com>
2024-02-02 00:09:15 -0600

    messing with form placement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,10 @@
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
+
 handleClick = () => {
   const bottomContainer = document.querySelector('.bottom-container')
   bottomContainer.scrollIntoView({behavior: "smooth"})
