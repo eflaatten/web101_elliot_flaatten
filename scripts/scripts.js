adf6d8df87b6cdb8ebc0c579df1f7d987f36fa69
eflaatten <elliotflaatten@live.com>
2024-02-04 19:37:34 -0600

    Tried adding red border on required fields. Some design tweaks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,16 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(document.createElement("br"))
 })
 
+// Function for adding red to required fields
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+  
+  let requiredInputs = document.querySelectorAll("[required]")
+
+  requiredInputs.style.border = "error-border"
+  console.log("Form submitted")
+})
+
 // about.html
 handleClick = () => {
   const bottomContainer = document.querySelector('.bottom-container')
